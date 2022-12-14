4d73491eab0d19f1b79cd16169100548d9daa2dc
Juliana da Cruz <juliana.da.cruz@avanade.com>
2022-12-13 21:12:32 -0300

    Iniciando com Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({                    //decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ListaTarefasDecolaDev';
-}
+export class AppComponent {              //exportando a classe AppComponent
+  tarefasArray:string[] = [];            //criando um array de string
+
+
+  adicionarTarefa(valor:string){               //criando um método para adicionar tarefas
+    this.tarefasArray.push(valor);             //adicionando o valor do input no array
+    console.log('Adicionando uma tarefa');
+    console.log(`Adiconando a tarefa ${valor}`);
+  }
+
+  removerTarefa(valor:string){              //criando um método para remover tarefas
+    for (let i = this.tarefasArray.length; i >= 0; i--) {
+      if (valor == this.tarefasArray[i]) {
+        this.tarefasArray.splice(i, 1);
+      }
+    }
+  }
+  constructor(){
+
+  }
+  }
+
